a04fc48ce717d1bf69d4e1239ec6fb5ca9e67c32
Alexia Nunes <alexiiianunes@gmail.com>
2020-09-06 09:09:17 -0300

    Inicio PHPUnit; Update migration de Contas

---
+++ database/migrations/2020_09_06_120434_update_contas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateContasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contas', function (Blueprint $table) {
+            $table->integer('conta')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ tests/Unit/SaqueTeste.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SaqueTeste extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_sacar()
+    {
+        $this->assertTrue(true);
+    }
+}
